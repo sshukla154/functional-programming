20df407b1a5e813546253daafd69a8b63854a2f1
Seemant Shukla <13106945+sshukla154@users.noreply.github.com>
2020-11-21 06:51:19 +0530

    Structural and Functional Approach With Filter

--- src/programming/FP01Functional.java
+++ src/programming/FP01Functional.java
@@ -6,31 +6,49 @@ public class FP01Functional {
 
 	public static void main(String[] args) {
 
-		printAllNumbersinListFunctional(List.of(10, 20, 54, 123, 87, 31, 01, 10, 65, 23));
+		List<Integer> numbers = List.of(10, 20, 54, 123, 87, 31, 01, 10, 65, 23);
+		printAllNumbersinListFunctional(numbers);
+		System.out.println("===");
+		printAllEvenNumbersinListFunctional(numbers);
 	}
 
 	private static void print(Integer number) {
 		System.out.println(number);
 	}
 
-	private static void printAllNumbersinListFunctional(List<Integer> numbers) {
+	private static void printAllNumbersinListFunctional_01(List<Integer> numbers) {
 		// Convert the number into stream of elements
 
 		System.out.println("Non-Static method reference---");
-		//Method reference for non-static method
+		// Method reference for non-static method
 		numbers.stream().forEach(number -> print(number));
-		
+
 		System.out.println();
-		
+
 		System.out.println("Static method reference---");
-		//Method reference for static method
+		// Method reference for static method
 		numbers.stream().forEach(FP01Functional::print);
-		
+
 		System.out.println();
-		
+
 		System.out.println("Static method reference---");
-		//Method reference for static method
+		// Method reference for static method
 		numbers.stream().forEach(System.out::println);
+	}
+
+	private static void printAllNumbersinListFunctional(List<Integer> numbers) {
+		numbers.stream()
+				.forEach(System.out::println);// Method Reference
+	}
+
+	private static boolean isEven(Integer number) {
+		return (number % 2 == 0);
+	}
+
+	private static void printAllEvenNumbersinListFunctional(List<Integer> numbers) {
+		numbers.stream()
+				.filter(FP01Functional::isEven)// Filter - Only allow even numbers
+				.forEach(System.out::println);// Method reference
 	}
 
 }
--- src/programming/FP01Structured.java
+++ src/programming/FP01Structured.java
@@ -5,14 +5,27 @@ import com.sun.tools.javac.util.List;
 public class FP01Structured {
 
 	public static void main(String[] args) {
+
+		List<Integer> numbers = List.of(10, 20, 54, 123, 87, 31, 01, 10, 65, 23);
+		printAllNumbersinListStructured(numbers);
 		
-		printAllNumbersinListStructured(List.of(10, 20, 54, 123,87, 31, 01, 10, 65, 23));
+		System.out.println("---");
+
+		printAllEvenNumbersinListStructured(numbers);
 	}
 
 	private static void printAllNumbersinListStructured(List<Integer> numbers) {
-		for(Integer number : numbers) {
+		for (Integer number : numbers) {
 			System.out.println(number);
 		}
 	}
-	
+
+	private static void printAllEvenNumbersinListStructured(List<Integer> numbers) {
+		for (Integer number : numbers) {
+			if (number % 2 == 0) {
+				System.out.println(number);
+			}
+		}
+	}
+
 }
